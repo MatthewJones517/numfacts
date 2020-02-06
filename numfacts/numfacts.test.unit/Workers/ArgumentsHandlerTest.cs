060c47d9c339185b7df2e129a891362ef9902e53
Matthew Jones <mail@matthewrjones.me>
2020-02-06 17:44:21 -0500

    Created ArgumentsHandler class with unit tests to handle user input.

---
+++ numfacts/numfacts.test.unit/Workers/ArgumentsHandlerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numfacts.Models;
+using numfacts.Workers;
+
+namespace numfacts.test.unit.Workers
+{
+    [TestClass]
+    public class ArgumentsHandlerTest
+    {
+        // Should throw an exception if at least one argument isn't passed in.
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ShouldThrowExceptionWhenNoArgumentsArePassedIn()
+        {
+            string[] args = new string[0];
+            bool argumentsModelIsValid = ArgumentsHandler.ArgumentsWerePassedIn(args);
+        }
+
+        // Should return true with at least one argument. 
+        [TestMethod]
+        public void ShouldReturnTrueWhenAtLeastOneArgumentIsPassedIn()
+        {
+            string[] args = new string[1] { "33" };
+            bool argumentsModelIsValid = ArgumentsHandler.ArgumentsWerePassedIn(args);
+
+            Assert.IsTrue(argumentsModelIsValid);
+        }
+
+        // Should throw an exception if we provide an invalid argument
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ShouldThrowExceptionWhenInvalidArgumentIsPassedIn()
+        {
+            string[] args = new string[3] { "33", "/m", "sdflkjdsfs" };
+            ArgumentsModel argumentsModel = ArgumentsHandler.CreateArgumentsModelFromUserInput(args);
+        }
+
+        // Should throw an exception if we put the number anywhere but the first argument.
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void ShouldThrowExceptionIfNumberIsNotFirstArgument()
+        {
+            string[] args = new string[2] { "/m", "33" };
+            ArgumentsModel argumentsModel = ArgumentsHandler.CreateArgumentsModelFromUserInput(args);
+        }
+
+        // Should create a model if a single number argument is passed in.
+        [TestMethod]
+        public void ShouldCreateModelWithSingleIntegerArgument()
+        {
+            string[] args = new string[1] { "33" };
+            ArgumentsModel argumentsModel = ArgumentsHandler.CreateArgumentsModelFromUserInput(args);
+
+            Assert.AreEqual(33, argumentsModel.Number);
+        }
+
+        // Should create a model if all possible arguments are passed in
+        [TestMethod]
+        public void ShouldCreateModelWithAllPossibleArguments()
+        {
+            string[] args = new string[4] { "33", "/r", "/m", "/t" };
+            ArgumentsModel argumentsModel = ArgumentsHandler.CreateArgumentsModelFromUserInput(args);
+
+            Assert.AreEqual(33, argumentsModel.Number);
+            Assert.IsTrue(argumentsModel.RandomNumber);
+            Assert.IsTrue(argumentsModel.MathFact);
+            Assert.IsTrue(argumentsModel.TriviaFact);
+        }
+    }
+}
---
+++ numfacts/numfacts/Workers/ArgumentsHandler.cs
@@ -0,0 +1,74 @@
+﻿using numfacts.Builders;
+using numfacts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numfacts.Workers
+{
+    static class ArgumentsHandler
+    {
+        public static bool ArgumentsWerePassedIn(string[] args)
+        {
+            // Check to make sure at least one argument has been passed in. If not, throw an exception.
+            if (args.Length == 0)
+            {
+                throw new Exception(Constants.HowToUse);
+            }
+
+            return true;
+        }
+        
+        public static ArgumentsModel CreateArgumentsModelFromUserInput(string[] args)
+        {
+            // Create the model builder and begin the building process. Check for errors along the way.
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+
+            // Loop through each argument and add it to the builder
+            for (int i = 0; i < args.Length; i++)
+            {
+                // If this is the first argument, try to parse it as an integer. 
+                bool numberSuccessfullyParsed = false;
+
+                if (i == 0)
+                {
+                    numberSuccessfullyParsed = int.TryParse(args[i], out int passedInNumber);
+
+                    if (numberSuccessfullyParsed)
+                    {
+                        argumentsModelBuilder.WithNumber(passedInNumber);
+                    }
+                }
+
+                // If the number didn't parse, or this isn't the first iteration of the loop,
+                // check for valid options and update model build accordingly.
+                if (!numberSuccessfullyParsed)
+                {
+                    switch (args[i])
+                    {
+                        case Constants.RandomNumberFlag:
+                            argumentsModelBuilder.WithRandomNumber();
+                            break;
+
+                        case Constants.MathFactFlag:
+                            argumentsModelBuilder.WithMathFact();
+                            break;
+
+                        case Constants.TriviaFactFlag:
+                            argumentsModelBuilder.WithTriviaFact();
+                            break;
+
+                        default:
+                            throw new Exception(Constants.InvalidInput + args[i]);
+                    }
+                }
+            }
+
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            return argumentsModel;
+        }
+    }
+}
