814e2a4d1b28988be84a5e51b81c4c0f341a1d05
Matthew Jones <mail@matthewrjones.me>
2020-02-06 15:42:55 -0500

    Created builder for ArgumentsModel with unit tests.

---
+++ numfacts/numfacts.test.unit/Builders/ArgumentsModelBuilderTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numfacts.Builders;
+using numfacts.Models;
+
+namespace numfacts.test.unit.Builders
+{
+    [TestClass]
+    public class ArgumentsModelBuilderTest
+    {
+        // Tests to make sure that the builder returns the proper defaults
+        [TestMethod]
+        public void ShouldCreateModelWithBuilderDefaults()
+        {
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            Assert.AreEqual(int.MinValue, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        // Tests to make sure that the builder properly stores a number
+        [TestMethod]
+        public void ShouldCreateModelWithNumber()
+        {
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+            argumentsModelBuilder.WithNumber(33);
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            Assert.AreEqual(33, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        // Tests to make sure that the builder properly stores the random number boolean
+        [TestMethod]
+        public void ShouldCreateModelWithRandomNumberBoolean()
+        {
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+            argumentsModelBuilder.WithRandomNumber();
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            Assert.AreEqual(int.MinValue, argumentsModel.Number);
+            Assert.AreEqual(true, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        // Tests to make sure that the builder properly stores the math fact boolean
+        [TestMethod]
+        public void ShouldCreateModelWithMathFactBoolean()
+        {
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+            argumentsModelBuilder.WithMathFact();
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            Assert.AreEqual(int.MinValue, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(true, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        // Tests to make sure that the builder properly stores the trivia fact boolean
+        [TestMethod]
+        public void ShouldCreateModelWithTriviaFactBoolean()
+        {
+            ArgumentsModelBuilder argumentsModelBuilder = new ArgumentsModelBuilder();
+            argumentsModelBuilder.WithTriviaFact();
+            ArgumentsModel argumentsModel = argumentsModelBuilder.Build();
+
+            Assert.AreEqual(int.MinValue, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(true, argumentsModel.TriviaFact);
+        }
+    }
+}
---
+++ numfacts/numfacts/Builders/ArgumentsModelBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using numfacts.Models;
+
+namespace numfacts.Builders
+{
+    // This class builds an instance of ArgumentsModel, which holds the user's passed in arguments. 
+    class ArgumentsModelBuilder
+    {
+        private int _number;
+        private bool _randomNumber;
+        private bool _mathFact;
+        private bool _triviaFact;
+
+        public ArgumentsModelBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _number = int.MinValue;
+            _randomNumber = false;
+            _mathFact = false;
+            _triviaFact = false;
+        }
+
+        public ArgumentsModelBuilder WithNumber(int num)
+        {
+            _number = num;
+            return this;
+        }
+
+        public ArgumentsModelBuilder WithRandomNumber()
+        {
+            _randomNumber = true;
+            return this;
+        }
+
+        public ArgumentsModelBuilder WithMathFact()
+        {
+            _mathFact = true;
+            return this;
+        }
+
+        public ArgumentsModelBuilder WithTriviaFact()
+        {
+            _triviaFact = true;
+            return this;
+        }
+        
+        public ArgumentsModel Build()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            argumentsModel.Number = _number;
+            argumentsModel.RandomNumber = _randomNumber;
+            argumentsModel.MathFact = _mathFact;
+            argumentsModel.TriviaFact = _triviaFact;
+
+            return argumentsModel;
+        }
+    }   
+}
