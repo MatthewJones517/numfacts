a7d7d7d3fe2f77c5e446a5fb478e9ced6f569443
Matthew Jones <mail@matthewrjones.me>
2020-02-06 17:43:18 -0500

    Created constants file to hold string literals.

--- numfacts/numfacts/Constants.cs
+++ numfacts/numfacts/Constants.cs
@@ -8,11 +8,20 @@ namespace numfacts
 {
     class Constants
     {
-        public const string HowToUse = "Get a random fact for a number you specify. Usage:\n" +
+        // Error Messages
+        public const string HowToUse = "Get a random fact for a whole number you specify. Usage:\n" +
                                        "numfacts [number] [attributes]" +
+                                       "Attributes: \n" +
                                        "\n\t /m - Request a math fact." +
                                        "\n\t /t - Request a trivia fact." +
                                        "\n\t /r - Choose a random number" +
                                        "\nIf no attributes are given a trivia fact is returned.";
+
+        public const string InvalidInput = "Invalid number or attribute: ";
+
+        // Attributes
+        public const string RandomNumberFlag = "/r";
+        public const string MathFactFlag = "/m";
+        public const string TriviaFactFlag = "/t";
     }
 }
