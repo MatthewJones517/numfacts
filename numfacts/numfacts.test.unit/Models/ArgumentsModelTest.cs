e02fbb765ab19d239a7d7e604d2c7baef751fbf8
Matthew Jones <mail@matthewrjones.me>
2020-02-06 14:17:44 -0500

    Created model to hold user arguments and created tests for it.

---
+++ numfacts/numfacts.test.unit/Models/ArgumentsModelTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numfacts.Models;
+
+namespace numfacts.test.unit.Models
+{
+    [TestClass]
+    public class ArgumentsModelTest
+    {
+        [TestMethod]
+        public void ShouldCreateEmptyModel()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            Assert.AreEqual(0, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        [TestMethod]
+        public void ShouldHoldNumberIntegerAndTriviaFactBoolean()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            argumentsModel.Number = 33;
+            argumentsModel.RandomNumber = false;
+            argumentsModel.MathFact = false;
+            argumentsModel.TriviaFact = true;
+
+            Assert.AreEqual(33, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(true, argumentsModel.TriviaFact);
+        }
+
+        [TestMethod]
+        public void ShouldHoldNumberIntegerAndMathFactBoolean()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            argumentsModel.Number = 33;
+            argumentsModel.RandomNumber = false;
+            argumentsModel.MathFact = true;
+            argumentsModel.TriviaFact = false;
+
+            Assert.AreEqual(33, argumentsModel.Number);
+            Assert.AreEqual(false, argumentsModel.RandomNumber);
+            Assert.AreEqual(true, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        [TestMethod]
+        public void ShouldHoldRandomNumberBooleanAndMathFactBoolean()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            argumentsModel.RandomNumber = true;
+            argumentsModel.MathFact = true;
+            argumentsModel.TriviaFact = false;
+
+            Assert.AreEqual(true, argumentsModel.RandomNumber);
+            Assert.AreEqual(true, argumentsModel.MathFact);
+            Assert.AreEqual(false, argumentsModel.TriviaFact);
+        }
+
+        [TestMethod]
+        public void ShouldHoldRandomNumberBooleanAndTriviaFactBoolean()
+        {
+            ArgumentsModel argumentsModel = new ArgumentsModel();
+
+            argumentsModel.RandomNumber = true;
+            argumentsModel.MathFact = false;
+            argumentsModel.TriviaFact = true;
+
+            Assert.AreEqual(true, argumentsModel.RandomNumber);
+            Assert.AreEqual(false, argumentsModel.MathFact);
+            Assert.AreEqual(true, argumentsModel.TriviaFact);
+        }
+    }
+}
---
+++ numfacts/numfacts/Models/ArgumentsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numfacts.Models
+{
+    class ArgumentsModel
+    {
+        public int Number { get; set; }
+        public bool RandomNumber { get; set; }
+        public bool MathFact { get; set; }
+        public bool TriviaFact { get; set; }
+    }
+}
