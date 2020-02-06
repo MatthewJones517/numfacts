9662d4cd2f0f03b26b54fdb168e73aa77ab36916
Matthew Jones <mail@matthewrjones.me>
2020-02-06 11:04:29 -0500

    Initial Commit

---
+++ numfacts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace numfacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
