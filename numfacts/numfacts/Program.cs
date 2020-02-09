5a84b10bc431d9ffc8d2c00cfaa0d0056021ce78
Matthew Jones <mail@matthewrjones.me>
2020-02-09 16:21:47 -0500

    Utilizing method to make sure passed in arguments are properly split apart.

--- numfacts/numfacts/Program.cs
+++ numfacts/numfacts/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using numfacts.Models;
+using numfacts.Workers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +14,23 @@ namespace numfacts
         {
             try
             {
+                // Make sure the arguments are properly split apart before creating the argument model.
+                string[] splitArgs = ArgumentsHandler.BreakApartArguments(args);
+                
+                // Create arguments model from passed in arguments. 
+                ArgumentsModel argumentsModel = ArgumentsHandler.CreateArgumentsModelFromUserInput(splitArgs);
+
+                // Validate the arguments model before making the API request.
+                ArgumentsHandler.ValidateArgumentsModel(argumentsModel);
+
+                // Make the API request. 
+
+                // Display the results to the user.
 
             } catch (Exception ex)
             {
-
+                // Display the error to the user.
+                Console.WriteLine(ex.Message);
             }
         }
     }
