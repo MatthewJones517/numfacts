bc6298f5fbf236d0f135205395208d7fcb34dfef
Matthew Jones <mail@matthewrjones.me>
2020-02-10 18:35:11 -0500

    Created APIResponseModelBuilder and tests.

---
+++ numfacts/numfacts.test.unit/Builders/APIResponseModelBuilderTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numfacts.Builders;
+using numfacts.Models;
+
+namespace numfacts.test.unit.Builders
+{
+    [TestClass]
+    public class APIResponseModelBuilderTest
+    {
+        [TestMethod]
+        public void ShouldCreateModelWithDefaultValues()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder.Build();
+
+            Assert.AreEqual(int.MinValue, apiResponseModel.Number);
+            Assert.AreEqual(string.Empty, apiResponseModel.Text);
+            Assert.AreEqual(string.Empty, apiResponseModel.Type);
+            Assert.IsFalse(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithNumberValue()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder.WithNumber(33).Build();
+
+            Assert.AreEqual(33, apiResponseModel.Number);
+            Assert.AreEqual(string.Empty, apiResponseModel.Text);
+            Assert.AreEqual(string.Empty, apiResponseModel.Type);
+            Assert.IsFalse(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithTextValue()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder.WithText("Random Fact").Build();
+
+            Assert.AreEqual(int.MinValue, apiResponseModel.Number);
+            Assert.AreEqual("Random Fact", apiResponseModel.Text);
+            Assert.AreEqual(string.Empty, apiResponseModel.Type);
+            Assert.IsFalse(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithTypeValue()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder.WithType("Math").Build();
+
+            Assert.AreEqual(int.MinValue, apiResponseModel.Number);
+            Assert.AreEqual(string.Empty, apiResponseModel.Text);
+            Assert.AreEqual("Math", apiResponseModel.Type);
+            Assert.IsFalse(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithFoundValue()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder.WithFound(true).Build();
+
+            Assert.AreEqual(int.MinValue, apiResponseModel.Number);
+            Assert.AreEqual(string.Empty, apiResponseModel.Text);
+            Assert.AreEqual(string.Empty, apiResponseModel.Type);
+            Assert.IsTrue(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateModelWithAllValues()
+        {
+            APIResponseModelBuilder apiResponseModelBuilder = new APIResponseModelBuilder();
+
+            APIResponseModel apiResponseModel = apiResponseModelBuilder
+                                                .WithText("Random Fact")
+                                                .WithType("Math")
+                                                .WithNumber(33)
+                                                .WithFound(true)
+                                                .Build();
+
+            Assert.AreEqual(33, apiResponseModel.Number);
+            Assert.AreEqual("Random Fact", apiResponseModel.Text);
+            Assert.AreEqual("Math", apiResponseModel.Type);
+            Assert.IsTrue(apiResponseModel.Found);
+        }
+    }
+}
---
+++ numfacts/numfacts/Builders/APIResponseModelBuilder.cs
@@ -0,0 +1,66 @@
+﻿using numfacts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numfacts.Builders
+{
+    class APIResponseModelBuilder
+    {
+        private string _text;
+        private int _number;
+        private bool _found;
+        private string _type;
+
+        public APIResponseModelBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _text = string.Empty;
+            _number = int.MinValue;
+            _found = false;
+            _type = string.Empty;
+        }
+
+        public APIResponseModelBuilder WithText(string text)
+        {
+            _text = text;
+            return this;
+        }
+
+        public APIResponseModelBuilder WithNumber(int number)
+        {
+            _number = number;
+            return this;
+        }
+
+        public APIResponseModelBuilder WithFound(bool found)
+        {
+            _found = found;
+            return this;
+        }
+
+        public APIResponseModelBuilder WithType (string type)
+        {
+            _type = type;
+            return this;
+        }
+
+        public APIResponseModel Build()
+        {
+            APIResponseModel responseModel = new APIResponseModel();
+
+            responseModel.Text = _text;
+            responseModel.Number = _number;
+            responseModel.Type = _type;
+            responseModel.Found = _found;
+
+            return responseModel;
+        }
+    }
+}
