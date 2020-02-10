83d95fcfef0dfb4879c22b374253a78950192138
Matthew Jones <mail@matthewrjones.me>
2020-02-10 18:01:56 -0500

    Added API Response model and tests.

---
+++ numfacts/numfacts.test.unit/Models/APIResponseModelTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using numfacts.Models;
+
+namespace numfacts.test.unit.Models
+{
+    [TestClass]
+    public class APIResponseModelTest
+    {
+        [TestMethod]
+        public void ShouldCreateEmptyModel()
+        {
+            APIResponseModel apiResponseModel = new APIResponseModel();
+
+            Assert.AreEqual(null, apiResponseModel.Text);
+            Assert.AreEqual(null, apiResponseModel.Type);
+            Assert.AreEqual(0, apiResponseModel.Number);
+            Assert.IsFalse(apiResponseModel.Found);
+        }
+
+        [TestMethod]
+        public void ShouldCreateFullModel()
+        {
+            APIResponseModel apiResponseModel = new APIResponseModel();
+
+            apiResponseModel.Found = true;
+            apiResponseModel.Text = "This is a random fact";
+            apiResponseModel.Type = "Math";
+            apiResponseModel.Number = 33;
+        }
+    }
+}
