fea7f9fb3f538dd91da47d926bac0142b742e23a
Matthew Jones <mail@matthewrjones.me>
2020-02-12 14:53:28 -0500

    Wrote client to retreive facts from the API.

---
+++ numfacts/numfacts/Workers/NumfactsAPIClient.cs
@@ -0,0 +1,49 @@
+﻿using numfacts.Constants;
+using numfacts.Models;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace numfacts.Workers
+{
+    static class NumfactsAPIClient
+    {
+        public static APIResponseModel GetNumFact(ArgumentsModel argumentsModel)
+        {
+            RestClient client = new RestClient(BuildAPIEndpointUrl(argumentsModel));
+            RestRequest request = new RestRequest(Method.GET);
+            request.AddHeader(APIConstants.HEADER_HOST_NAME, APIConstants.HEADER_HOST_VALUE);
+            request.AddHeader(APIConstants.HEADER_KEY_NAME, APIConstants.HEADER_KEY_VALUE);
+
+            IRestResponse<APIResponseModel> response = client.Execute<APIResponseModel>(request);
+
+            return response.Data;
+        }
+
+        private static string BuildAPIEndpointUrl(ArgumentsModel argumentsModel)
+        {
+            string returnValue = APIConstants.BASE_URL;
+
+            if (argumentsModel.RandomNumber)
+            {
+                returnValue += APIConstants.RANDOM_NUMBER_ENDPOINT;
+            } else
+            {
+                returnValue += argumentsModel.Number.ToString();
+            }
+
+            returnValue += "/";
+
+            if (argumentsModel.MathFact)
+            {
+                returnValue += APIConstants.MATH_FACT_ENDPOINT;
+            } else
+            {
+                returnValue += APIConstants.TRIVIA_FACT_ENDPOINT;
+            }
+
+            returnValue += APIConstants.JSON_AND_FRAGMENT_ENDPOINT;
+
+            return returnValue;
+        }
+    }
+}
