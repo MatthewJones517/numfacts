4d2821b1eba487c4aa6b5fefee8a3e9414378bf3
Matthew Jones <mail@matthewrjones.me>
2020-02-12 14:53:10 -0500

    Updated APIResponseModel so it could be automatically deserialized.

--- numfacts/numfacts/Models/APIResponseModel.cs
+++ numfacts/numfacts/Models/APIResponseModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp.Deserializers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,16 @@ namespace numfacts.Models
 {
     class APIResponseModel
     {
+        [DeserializeAs(Name = "text")]
         public string Text { get; set; }
+
+        [DeserializeAs(Name = "number")]
         public int Number { get; set; }
+
+        [DeserializeAs(Name = "found")]
         public bool Found { get; set; }
+
+        [DeserializeAs(Name = "type")]
         public string Type { get; set; }
     }
 }
