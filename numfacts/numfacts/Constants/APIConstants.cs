779f0854d82198cfee419d46066e5b952610d31e
Matthew Jones <mail@matthewrjones.me>
2020-02-13 13:15:18 -0500

    Added updated comments about the API constants.

--- numfacts/numfacts/Constants/APIConstants.cs
+++ numfacts/numfacts/Constants/APIConstants.cs
@@ -9,11 +9,13 @@ namespace numfacts.Constants
     class APIConstants
     {
         // Header values. You will need to replace API_HEADER_KEY_VALUE with your own api key.
-        public const string HEADER_KEY_NAME = "X-RapidAPI-Key";
-        public const string HEADER_KEY_VALUE = "22f4edda2amshe3183f60ad0df3bp13dc9fjsn026d79530589";
-
         public const string HEADER_HOST_NAME = "X-RapidAPI-Host";
         public const string HEADER_HOST_VALUE = "numbersapi.p.rapidapi.com";
+
+        // Yes, I know it's a bad idea to commit your API key to a public repository. I'm doing it because
+        // I don't want you to be hassled by having to sign up for your own key if you're reviewing the code.
+        public const string HEADER_KEY_NAME = "X-RapidAPI-Key";
+        public const string HEADER_KEY_VALUE = "22f4edda2amshe3183f60ad0df3bp13dc9fjsn026d79530589";
 
         // Endpoint Strings
         public const string BASE_URL = "https://numbersapi.p.rapidapi.com";
