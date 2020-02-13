85adcb431d8782b9412d76831660391d707b7c19
Matthew Jones <mail@matthewrjones.me>
2020-02-13 11:29:37 -0500

    Created OutputConstants file to hold strings displayed to use user when the response is created.

---
+++ numfacts/numfacts/Constants/OutputConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numfacts.Constants
+{
+    class OutputConstants
+    {
+        // Constants used for displaying output to the user.
+        public const string FACT_DISPLAY_TEMPLATE = "{0} {1} is {2}.";
+        public const string MATH_FACT_PREFIX = "MATH FACT: ";
+        public const string TRIVIA_FACT_PREFIX = "TRIVIA FACT: ";
+        public const string API_LOADING = "\nRequesting your fact from the API. Please wait...\n";
+    }
+}
